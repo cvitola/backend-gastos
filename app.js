5a0ccf7184e46ca161f50f6c3816d3e94f443fa5
Cristian <isrunaway@gmail.com>
2022-07-24 20:34:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//const spendRouter = require("./src/routes/spend");
+
+const app = express();
+app.use(bodyParser.json());
+
+//app.use('/api/spends', spendRouter);
+app.use( ( req, res, next ) => {
+    res.statusCode = 404;
+
+    res.send("Hola Mundo");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`🚀 Server ready at: http://localhost:${PORT} ⭐️`)
+);
