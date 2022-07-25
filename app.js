41b57d3583bab2dc625a7ac420a6a948a1ef7cf3
Cristian <isrunaway@gmail.com>
2022-07-25 18:55:37 -0300

    Quito conexion Mongo

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3000;
 
 const main = async () => {
   try {
-    await connectMongo();
+    //await connectMongo();
     app.listen(PORT, () =>
       console.log(`🚀 Server ready at: http://localhost:${PORT} ⭐️`)
     );
