acaf6967f9a4411407d35cc24491925ae498b185
Cristian <isrunaway@gmail.com>
2022-07-27 14:31:08 -0300

    Cambios en el Controller

--- src/controllers/spend.js
+++ src/controllers/spend.js
@@ -14,31 +14,32 @@ const getAllSpends = async (req,res,next) => {
 
 const createSpend = async (req,res,next) => {
     console.log("Creating data: ", req.body);
-    if(req.body.date === "") {
-        res.statusCode = 400;
-        res.send("Field date is empty.")
-        console.log("Field Date cannot be empty")
-    }
-    if(req.body.category === "") {
-        res.statusCode = 400;
-        res.send("Field category is empty.")
-        console.log("Field Category cannot be empty")
-    }
-    if(req.body.amount === "") {
-        res.statusCode = 400;
-        res.send("Field amount is empty.")
-        console.log("Field Amount cannot be empty")
-    }
-    if(res.statusCode != 400) {
-        try {
+    try{
+        if(req.body.date === "") {
+            res.statusCode = 400;
+            res.send("Field date is empty.")
+            console.log("Field Date cannot be empty")
+        }
+        if(req.body.category === "") {
+            res.statusCode = 400;
+            res.send("Field category is empty.")
+            console.log("Field Category cannot be empty")
+        }
+        if(req.body.amount === "") {
+            res.statusCode = 400;
+            res.send("Field amount is empty.")
+            console.log("Field Amount cannot be empty")
+        }
+        if(res.statusCode != 400) {
             const newSpend = await spend.createSpend(req.body.date, req.body.type, req.body.amount);
             res.send(newSpend)
-        } catch (error) {
-            console.log(error);
-            res.statusCode = 500;
-            res.send(error.message)
-    }  
+        }
+    } catch(error){
+        console.log(error);
+        res.statusCode = 500;
+        res.send(error.message)
     }
+
 
 
 }
